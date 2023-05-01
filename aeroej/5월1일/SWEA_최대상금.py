cff778fab20581c0bff995a11527d0a4927aee85
Eunjin Kim <asd0755@naver.com>
2023-05-01 23:06:27 +0900

    Create SWEA_최대상금.py

---
+++ aeroej/5월1일/SWEA_최대상금.py
@@ -0,0 +1,40 @@
+from collections import defaultdict
+visited = defaultdict(list)
+
+def dfs(board, cnt):
+  global ans
+  if cnt == 0:
+    return
+  
+  for i in range(l-1):
+    for j in range(i+1, l):
+      board[i], board[j] = board[j], board[i]
+      new_num = int(''.join(board))
+      
+      #방문한 적 없거나, 방문한 적 있지만 현재 교환 횟수가 더 많이 남아있는 경우
+      if not len(visited[new_num]) or visited[new_num][1] < cnt:
+        visited[new_num] = [0, cnt]
+
+        if cnt%2 == 0:
+          #일의 자리와 십의 자리 교환
+          new_num = new_num//100*100 + new_num%10*10 + new_num//10%10
+        
+        ans = max(ans, new_num)
+        dfs(board, cnt-1)
+        
+      board[i], board[j] = board[j], board[i]
+      
+      
+t = int(input())
+
+for test_case in range(1, t+1):
+  n, m = input().split()
+  l = len(n)
+  m = int(m)
+
+  init_board = [_ for _ in n]
+  ans = 0
+  
+  dfs(init_board, m)
+  print('#{0} {1}'.format(test_case,ans))
+
